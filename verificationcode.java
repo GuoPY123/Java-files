8c199cac1813736d6bff9b52dca2eb83db912e67
P.Y. Guo <90055265+GuoPY123@users.noreply.github.com>
2022-06-23 00:39:17 +0800

    Update verificationcode.java

--- verificationcode.java
+++ verificationcode.java
@@ -1,5 +1,17 @@
 public class verificationcode{
     public static void main(String[]args){
-        
+        int a = 1;
+        String ab = "";
+        for(byte c = 00;c < 04;c++){
+            ab = "";
+            for(int i = 0;i < 6;i++){
+                a += Math.random()*5;
+                if (a >= 10){
+                    a -= 6;
+                }
+                ab = ab + a;
+            }
+            System.out.println(ab);
+        }
     }
 }
